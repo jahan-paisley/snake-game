2b35cfe5ed129ddb414a682ca090d8220c234f21
Jahangir Zinedine <jzinedine@gmail.com>
2016-10-26 01:14:24 +0330

    initial commit started with a sample netty chat server

---
+++ src/main/java/com/theweirdscience/demo/SecureGameServer.java
@@ -0,0 +1,47 @@
+package com.theweirdscience.demo;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author <a href="mailto:norman.maurer@googlemail.com">Norman Maurer</a>
+ */
+public class SecureGameServer extends GameServer {
+
+    private final SslContext context;
+
+    public SecureGameServer(SslContext context) {
+        this.context = context;
+    }
+
+    @Override
+    protected ChannelInitializer<Channel> createInitializer(ChannelGroup group) {
+        return new SecureGameServerIntializer(group, context);
+    }
+
+    public static void main(String[] args) throws Exception{
+        if (args.length != 1) {
+            System.err.println("Please give port as argument");
+            System.exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+        SelfSignedCertificate cert = new SelfSignedCertificate();
+        SslContext context = SslContext.newServerContext(cert.certificate(), cert.privateKey());
+        final SecureGameServer endpoint = new SecureGameServer(context);
+        ChannelFuture future = endpoint.start(new InetSocketAddress(port));
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                endpoint.destroy();
+            }
+        });
+        future.channel().closeFuture().syncUninterruptibly();
+    }
+}
---
+++ src/main/java/com/theweirdscience/demo/SecureGameServerIntializer.java
@@ -0,0 +1,28 @@
+package com.theweirdscience.demo;
+
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+import javax.net.ssl.SSLEngine;
+
+/**
+ * @author <a href="mailto:norman.maurer@googlemail.com">Norman Maurer</a>
+ */
+public class SecureGameServerIntializer extends GameServerInitializer {
+    private final SslContext context;
+
+    public SecureGameServerIntializer(ChannelGroup group, SslContext context) {
+        super(group);
+        this.context = context;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        super.initChannel(ch);
+        SSLEngine engine = context.newEngine(ch.alloc());
+        engine.setUseClientMode(false);
+        ch.pipeline().addFirst(new SslHandler(engine));
+    }
+}
